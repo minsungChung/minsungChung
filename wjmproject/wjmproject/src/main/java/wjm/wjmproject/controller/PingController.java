60a0ea1d6a72c2738981894485dfcde36891ec98
minsungChung <als0318tjd@gmail.com>
2022-11-04 12:53:16 +0900

    2

--- wjmproject/wjmproject/src/main/java/wjm/wjmproject/controller/PingController.java
+++ wjmproject/wjmproject/src/main/java/wjm/wjmproject/controller/PingController.java
@@ -1,14 +1,12 @@
 package wjm.wjmproject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PingController {
 
-    @GetMapping("/ping/{id}")
-    public String Ping(@PathVariable String id){
-        return id;
+    @RequestMapping(value = "/test/{user}", method = RequestMethod.POST)
+    public String Ping(@PathVariable String user){
+        return user;
     }
 }
